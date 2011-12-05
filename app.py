9c362f1fa047cd3b78809c1a37fee0d785f4e4f4
Ivan Kolodyazhny <e0ne@e0ne.info>
2011-12-05 13:36:00 +0200

    UI updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request
 
 import twits
 
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', twits=twits.favorite_twits())
 
+@app.route('/unfavorite')
+def unfavorite():
+    return jsonify(result='ok')
 
 if __name__ == '__main__':
     app.debug = True
--- static/js/links.js
+++ static/js/links.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
-    $(".unfaforite_twit").click(function(){
-        alert('ssss');
+    $('.unfavorite_twit').click(function(){
+        $(this).attr('src', 'https://si0.twimg.com/images/dev/cms/intents/icons/favorite.png');
+        $.get('/unfavorite?t=twitter&id=2', null, function(){
+            alert('done');
+        });
+        return false;
     })
 });
